09730211283068f42d43c8aab1ebb0ffcd9d6827
kwservices12 <k.w.services12@gmail.com>
2021-09-23 14:33:37 -0400

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>logo</h1>
+        </div>
+      </div>
+    );
+    
+
+  }
+}
